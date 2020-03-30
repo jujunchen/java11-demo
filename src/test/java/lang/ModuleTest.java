514d617ee760b8c605a5677960a45a46d66ec595
chenjujun <1913210361@qq.com>
2020-03-30 13:08:56 +0800

    Module

--- src/test/java/lang/ModuleTest.java
+++ src/test/java/lang/ModuleTest.java
@@ -1,8 +1,49 @@
 package lang;
 
+import org.junit.Test;
+
+import java.lang.module.ModuleDescriptor;
+import java.util.List;
+
 /**
  * @author jujun chen
  * @date 2020/03/29
  */
 public class ModuleTest {
+
+    /**
+     * 如果模块是命名模块，返回true
+     */
+    @Test
+    public void isNamed() {
+        Module module = String.class.getModule();
+        assert module.isNamed();
+    }
+
+    /**
+     * 返回模块名称
+     */
+    @Test
+    public void getName() {
+        Module module = String.class.getModule();
+        System.out.println(module.getName());
+    }
+
+    /**
+     * 返回加载模块ClassLoader
+     */
+    @Test
+    public void getClassLoader() {
+        Module module = List.class.getModule();
+        ClassLoader classLoader = module.getClassLoader();
+    }
+
+    /**
+     * 返回此模块的描述符
+     */
+    @Test
+    public void getDescriptor() {
+        Module module = List.class.getModule();
+        ModuleDescriptor moduleDescriptor = module.getDescriptor();
+    }
 }
