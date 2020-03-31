4649a9009a5cdf275eaedcba4bae3fa1a11f0c73
chenjujun <1913210361@qq.com>
2020-03-31 23:18:32 +0800

    Module

--- src/test/java/lang/ModuleTest.java
+++ src/test/java/lang/ModuleTest.java
@@ -46,4 +46,42 @@ public class ModuleTest {
         Module module = List.class.getModule();
         ModuleDescriptor moduleDescriptor = module.getDescriptor();
     }
+
+    /**
+     * 返回包含此模块的模块层。
+     * 如果此模块不在模块层中，返回null。
+     *
+     * 模块层只包含命名模块，如果在未命名模块上调用，返回null
+     */
+    @Test
+    public void getLayer() {
+        Module module = String.class.getModule();
+        ModuleLayer moduleLayer = module.getLayer();
+        System.out.println(moduleLayer.toString());
+    }
+
+    /**
+     * 表示此模块是否读取给定模块。
+     * 如果调用此方法读取自身，则此方法返回true 。
+     * 如果在未命名的模块上调用它也会返回true （因为未命名的模块读取所有模块）
+     */
+    @Test
+    public void canRead() {
+        Module module = String.class.getModule();
+        Module module1 = List.class.getModule();
+
+        System.out.println(module.canRead(module1));
+    }
+
+    /**
+     * 如果调用者模块与当前模块相同，则更新当前模块对应的值为指定模块。
+     * 如果指定模块与当前模块相同，则不做任何操作
+     *
+     */
+    @Test
+    public void addReads() {
+        Module module = String.class.getModule();
+        Module module1 = List.class.getModule();
+        module.addReads(module1);
+    }
 }
