3895b8af2fed02adada481e8a1de600a4020f0ae
chenjujun <1913210361@qq.com>
2020-04-11 23:05:48 +0800

    add RuntimeTest

--- src/test/java/lang/RuntimeTest.java
+++ src/test/java/lang/RuntimeTest.java
@@ -1,8 +1,33 @@
 package lang;
 
+import org.junit.Test;
+
 /**
  * @author jujun chen
  * @date 2020/04/11
  */
 public class RuntimeTest {
+
+    /**
+     * 返回当前程序关联的运行时对象
+     */
+    @Test
+    public void getRuntime() {
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println(runtime.getClass());
+    }
+
+    /**
+     * 终止当前运行的虚拟机
+     * 同 System.exit
+     * 非零表示立即停止
+     */
+    @Test
+    public void exit() throws InterruptedException {
+        new Thread(() -> {
+            Runtime.getRuntime().exit(-1);
+        }).start();
+
+        Thread.sleep(3000);
+    }
 }
