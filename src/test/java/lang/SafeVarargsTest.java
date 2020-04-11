ca59edd53ad58086dbe468eebd01a968e232eef7
chenjujun <1913210361@qq.com>
2020-04-11 21:58:23 +0800

    add SafeVarargsTest

---
+++ src/test/java/lang/SafeVarargsTest.java
@@ -0,0 +1,40 @@
+package lang;
+
+/**
+ * @author jujun chen
+ * @date 2020/04/11
+ */
+public class SafeVarargsTest<T> {
+
+    private T[] args;
+
+    @SafeVarargs
+    public SafeVarargsTest(T... args) {
+    }
+
+    @SuppressWarnings("unchecked")
+//    @SafeVarargs //不能使用，方法未声明为static 或final
+    public  void loopArgs(T... args) {
+        for (T arg : args) {
+            System.out.println(arg);
+        }
+    }
+
+
+    @SafeVarargs
+    public final void finalArgs(T... args) {
+
+    }
+
+    @SafeVarargs
+    public static <T> void staticArgs(T... args) {
+
+    }
+
+    @SafeVarargs
+    private void privateArgs(T... args) {
+
+    }
+
+    
+}
