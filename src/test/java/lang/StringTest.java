92156a85e94f05789b7ccc110c5ec56ca0a294b5
chenjujun <1913210361@qq.com>
2020-04-15 23:54:48 +0800

    add StringTest

--- src/test/java/lang/StringTest.java
+++ src/test/java/lang/StringTest.java
@@ -2,6 +2,8 @@ package lang;
 
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * @author jujun chen
  * @date 2020/04/14
@@ -9,7 +11,7 @@ import org.junit.Test;
 public class StringTest {
     
     @Test
-    public void String() {
+    public void String() throws UnsupportedEncodingException {
         String abc = "abc";
         String string = new String(abc.getBytes());
         System.out.println(string);
@@ -19,7 +21,57 @@ public class StringTest {
         String string1 = new String(chars,1,1);
         System.out.println(string1);
 
+        //代码点
+        String string2 = new String(new int[]{97},0,1);
+        System.out.println(string2);
 
+        String string3 = new String(abc.getBytes(),"UTF-8");
+        System.out.println(string3);
+
+        //StringBuffer
+        StringBuffer stringBuffer = new StringBuffer("abc");
+        String string4 = new String(stringBuffer);
+        System.out.println(string4);
+        //修改stringBuffer，原来的字符串不变
+        stringBuffer.append("d");
+        System.out.println(string4);
+
+        //StringBuilder
     }
+
+    /**
+     * 返回字符串的长度
+     */
+    @Test
+    public void length() {
+        System.out.println("abc".length());
+    }
+
+    /**
+     * 当length()的长度为0时返回true
+     */
+    @Test
+    public void isEmpty() {
+        assert "".isEmpty();
+    }
+
+    /**
+     * 返回指定索引处的char值
+     */
+    @Test
+    public void charAt() {
+        char ch = "abc".charAt(1);
+        System.out.println(ch);
+    }
+
+    /**
+     * 返回指定索引处的代码点
+     */
+    @Test
+    public void codePointAt() {
+        int codePoint = "abc".codePointAt(1);
+        System.out.println(codePoint);
+    }
+
 
 }
