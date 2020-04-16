99d09e2be517f5e0862ae3569714654526bbcd91
chenjujun <1913210361@qq.com>
2020-04-16 23:36:09 +0800

    add StringTest

--- src/test/java/lang/StringTest.java
+++ src/test/java/lang/StringTest.java
@@ -143,4 +143,106 @@ public class StringTest {
     public void equalsIgnoreCase() {
         assert "ABC".equalsIgnoreCase("abc");
     }
+
+    /**
+     * 按字典顺序比较两个字符串
+     * 此字符串对象排在参数字符串之前，返回负数
+     * 此字符串对象排在参数字符串之后，返回正数
+     * 相等，返回零
+     */
+    @Test
+    public void compareTo() {
+        System.out.println("abc".compareTo("abd"));
+        System.out.println("abc".compareTo("ab"));
+        //如果长度相同,根据不同字符的字段顺序比较
+        System.out.println("abcd".compareTo("abdc"));
+        System.out.println("aBc".compareTo("abc"));
+
+        //忽略大小写
+        System.out.println("aBc".compareToIgnoreCase("abc"));
+    }
+
+    /**
+     * 将此字符串对象的子字符串与参数的字符串进行比较，如果子字符串相同，则结果为true，否则为false。
+     * 要比较的此String对象的子字符串从索引toffset开始，长度为len ,要比较的其他子串从索引ooffset开始，长度为len
+     * 参数
+     * toffset - 此字符串 toffset区域的起始偏移量。
+     * other - 字符串参数。
+     * ooffset - 字符串参数 ooffset区域的起始偏移量。
+     * len - 要比较的字符数。
+     */
+    @Test
+    public void regionMatches() {
+        System.out.println("abc".regionMatches(1,"abc",1,2));
+        //比较忽略大小写
+        System.out.println("Abc".regionMatches(true,1,"abc",1,2));
+    }
+
+    /**
+     * 测试从指定索引开始的子字符串是否已指定前缀开头
+     */
+    @Test
+    public void startsWith() {
+        System.out.println("abc".startsWith("ab",0));
+        System.out.println("abc".startsWith("ab"));
+    }
+
+    /**
+     * 测试此字符串是否已指定的后缀结尾
+     */
+    @Test
+    public void endsWith() {
+        System.out.println("abc".endsWith("bc"));
+    }
+
+    /**
+     * 返回字符串中第一次出现指定字符的索引，如果未出现该字符返回-1
+     */
+    @Test
+    public void indexOf() {
+        System.out.println("abc".indexOf('a'));
+        System.out.println("abc".indexOf("d"));
+        System.out.println("abc".indexOf(97));
+        System.out.println("abc".indexOf("ab"));
+
+        System.out.println("abc".indexOf('c',1));
+        System.out.println("abc".indexOf("c",1));
+
+        //fromIndex 为负数，表示从0开始
+        System.out.println("abc".indexOf('c', -2));
+
+        //fromIndex大于等于字符串长度，则fromIndex为字符串长度,返回-1
+        System.out.println("abc".indexOf('c',3));
+    }
+
+    /**
+     * 返回指定字符最后一次出现的字符串中的索引,向后搜索
+     */
+    @Test
+    public void lastIndexOf() {
+        System.out.println("aba".lastIndexOf('a'));
+        System.out.println("aba".lastIndexOf("a"));
+
+        //从fromIndex指定的索引处开始搜索，如果是负数，返回-1
+        System.out.println("abc".lastIndexOf('c',1));
+        System.out.println("abc".lastIndexOf("c",1));
+    }
+
+    /**
+     * 返回子字符串，子字符串以指定索引处的字符开头，并延伸到此字符串的末尾。
+     */
+    @Test
+    public void substring() {
+        assert "abc".substring(1).equals("bc");
+        assert "abc".substring(3).equals("");
+
+        //返回一个字符串，该字符串是此字符串的子字符串。
+        // 子字符串从指定的beginIndex开始，并扩展到索引endIndex - 1处的字符。
+        // 因此子串的长度是endIndex-beginIndex
+        assert "hamburger".substring(4, 8).equals("urge");
+
+        //返回作为此序列的子序列的字符序列
+        CharSequence charSequence = "hamburger".subSequence(4, 8);
+    }
+
 }
