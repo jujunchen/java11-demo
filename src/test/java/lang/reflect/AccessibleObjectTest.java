dffcaf18d0de709b31421c8a1e99a8889647715c
jujunchen <1913210361@qq.com>
2020-05-01 15:00:26 +0800

    add AccessibleObjectTest

--- src/test/java/lang/reflect/AccessibleObjectTest.java
+++ src/test/java/lang/reflect/AccessibleObjectTest.java
@@ -1,8 +1,27 @@
 package lang.reflect;
 
+import lang.Person;
+import org.junit.Test;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
+
 /**
  * @author jujun chen
  * @date 2020/05/01
  */
 public class AccessibleObjectTest {
+    
+    @Test
+    public void setAccessible() throws IllegalAccessException {
+        //对数组中的每个元素这只accessible标志
+        Person person = new Person();
+        person.setAddress("浙江杭州");
+        Field[] fields = person.getClass().getDeclaredFields();
+        //没有设置ture，获取address 字段时将报IllegalAccessException
+        AccessibleObject.setAccessible(fields, true);
+        for (Field field : fields) {
+            System.out.println(field.get(person));
+        }
+    }
 }
