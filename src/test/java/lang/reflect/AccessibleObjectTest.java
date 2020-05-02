a8117b14251e9b3e0d27ba0a46265954dac7bf3d
chenjujun <1913210361@qq.com>
2020-05-02 23:46:12 +0800

    add AccessibleObjectTest

--- src/test/java/lang/reflect/AccessibleObjectTest.java
+++ src/test/java/lang/reflect/AccessibleObjectTest.java
@@ -13,7 +13,7 @@ import java.lang.reflect.Field;
 public class AccessibleObjectTest {
     
     @Test
-    public void setAccessible() throws IllegalAccessException {
+    public void setAccessible() throws IllegalAccessException, NoSuchFieldException {
         //对数组中的每个元素这只accessible标志
         Person person = new Person();
         person.setAddress("浙江杭州");
@@ -23,5 +23,35 @@ public class AccessibleObjectTest {
         for (Field field : fields) {
             System.out.println(field.get(person));
         }
+
+        Person person1 = new Person();
+        person1.setAddress("绍兴诸暨");
+        Field field = person1.getClass().getDeclaredField("address");
+        field.setAccessible(true);
+        System.out.println(field.get(person1));
+    }
+    
+    
+    @Test
+    public void trySetAccessible() throws NoSuchFieldException, IllegalAccessException {
+        Person person1 = new Person();
+        person1.setAddress("绍兴诸暨");
+        Field field = person1.getClass().getDeclaredField("address");
+        if (field.trySetAccessible()) {
+            System.out.println(field.get(person1));
+        } else {
+            System.out.println("111");
+        }
+    }
+
+
+    @Test
+    public void canAccess() throws NoSuchFieldException {
+        Person person1 = new Person();
+        person1.setAddress("绍兴诸暨");
+        Field field = person1.getClass().getDeclaredField("address");
+        //未设置，无法访问私有属性
+//        field.setAccessible(true);
+        System.out.println(field.canAccess(person1));
     }
 }
