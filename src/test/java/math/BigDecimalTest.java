803ebfb73d7bf08cf4c53f1bb13bb5b509e86e7a
chenjujun <1913210361@qq.com>
2020-05-22 10:14:14 +0800

    add BigDecimalTest

--- src/test/java/math/BigDecimalTest.java
+++ src/test/java/math/BigDecimalTest.java
@@ -21,6 +21,20 @@ public class BigDecimalTest {
 
         BigDecimal bigDecimal2 = new BigDecimal(new char[]{'1','2','.','5'}, 1, 3, MathContext.DECIMAL32);
         System.out.println(bigDecimal2.toString());
+
+        MathContext mathContext = new MathContext(3);
+        BigDecimal bigDecimal3 = new BigDecimal("12.55", mathContext);
+        System.out.println(bigDecimal3.toString());
+
+        BigDecimal bigDecimal4 = new BigDecimal(0.1);
+        BigDecimal bigDecimal5 = new BigDecimal("0.1");
+        BigDecimal bigDecimal6 = new BigDecimal(Double.toString(0.1));
+        BigDecimal bigDecimal7 = new BigDecimal(Double.valueOf(0.1));
+
+        System.out.println(bigDecimal4);
+        System.out.println(bigDecimal5);
+        System.out.println(bigDecimal6);
+        System.out.println(bigDecimal7);
     }
 
 }
